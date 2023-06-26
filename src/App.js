8eba406f10cf22ff52f1371b78e2eb3734711024
kakakaqo <alsdn481@naver.com>
2023-06-26 18:01:28 +0900

    예제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import {AppBar, Toolbar, Typography} from '@mui/material';
 import './App.css';
+import UserList from './component/UserList';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AppBar position='static'>
+      <Toolbar>
+        <Typography variant='h6'>
+          User List
+        </Typography>
+      </Toolbar>
+     </AppBar>
+     <UserList />
     </div>
   );
 }
-
-export default App;
