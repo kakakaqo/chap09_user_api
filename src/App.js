b94ec8311e2caab9421437e1e026498a683ae3f6
kakakaqo <alsdn481@naver.com>
2023-06-27 14:39:32 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
-import {AppBar, Toolbar, Typography} from '@mui/material';
-import './App.css';
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, Button, Snackbar } from '@mui/material';
 import UserList from './component/UserList';
+import Login from './component/Login';
 
 export default function App() {
+  // 처리 결과 메시지를 보여주고 내리는 역할을 하는 상태
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  // 메시지 내용 변경 상태
+  const [snackbarMessage, setSnackbarMessage] = useState('');
+  // 로그인한 사용자 정보 저장용 상태변수
+  const [loggedInUser, setLoggedInUser] = useState(null);
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
+  // Login 컴포넌트에서 로그인에 성공한 후 호출됨.
+  const handleLogin = (user) => {
+    alert('handleLogin');
+    console.log('handleLogin', user);
+    setLoggedInUser(user); // 로그인한 사용자 정보 세팅(변경)
+    setSnackbarMessage(`${user.name}님 환영합니다`);
+    setSnackbarOpen(true);
+  };
+  // 로그아웃 함수
+  const handleLogout = () => {
+    setLoggedInUser(null);
+    setSnackbarMessage("로그아웃 되었습니다.");
+    setSnackbarOpen(true);
+  };
+
   return (
     <div className="App">
-     <AppBar position='static'>
-      <Toolbar>
-        <Typography variant='h6'>
-          User List
-        </Typography>
-      </Toolbar>
-     </AppBar>
-     <UserList />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>
+            User List
+          </Typography>
+          {loggedInUser ? (
+            <>
+              <Typography variant="h6" style={{ marginRight: '10px' }}>
+                {loggedInUser.name}님
+              </Typography>
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Login
+              setSnackbarOpen={setSnackbarOpen}
+              setSnackbarMessage={setSnackbarMessage}
+              onLogin={handleLogin}
+            />
+          )}
+        </Toolbar>
+      </AppBar>
+      <UserList />
+      {/* 메시지를 띄워주는 역할 open={true}되면 떠오름 */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
+      />
     </div>
   );
 }
---
+++ src/component/Login.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField } from '@mui/material';
+import { SERVER_URL } from "../constant";
+/**
+ * 로그인 컴포넌트
+ * @param {*} param0 
+ * @returns 
+ */
+export default function Login({ setSnackbarOpen, setSnackbarMessage, onLogin }) {
+  // 로그인 다이얼로그 오픈/내림을 결정하는 상태 변수
+  const [open, setOpen] = useState(false);
+  // 사용자 정보 저장용 상태 변수
+  const [loginUser, setLoginUser] = useState({
+    id: '',
+    password: ''
+  });
+
+  // 로그인 다이얼로그 오픈
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  // 로그인 다이얼로그 내림
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // 로그인 처리(Api Server에 요청)
+  const handleLogin = () => {
+    fetch(SERVER_URL + 'login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // 사용자 정보를 json 타입으로 변환해서 body에 넣어서 보냄.
+      body: JSON.stringify(loginUser), 
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else if (response.status === 401) {
+          throw new Error('일치하는 사용자가 없습니다');
+        } else {
+          throw new Error(response.status + ' ' + response.statusText);
+        }
+      })
+      .then(data => {
+        if (data.id) { // 사용자 정보가 있을경우 즉, 로그인 성공
+          setOpen(false);
+          onLogin(data); // App component의 onLogin함수 호출
+          setSnackbarMessage(`${data.name}님 환영합니다`);
+          setSnackbarOpen(true);
+        } else {
+          setSnackbarMessage(data);
+          setSnackbarOpen(true);
+        }
+      })
+      .catch(err => {
+        setSnackbarMessage(err.message);
+        setSnackbarOpen(true);
+        console.error(err);
+      });
+  };
+
+  return (
+    <>
+      {/* 로그인 텍스트 */}
+      <Button color="inherit" onClick={handleOpen}>Login</Button>
+      {/* 로그인 다이얼로그 
+        onClose={handleClose} : 대화상자가 닫힐때 호출되는 함수 */}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Login</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="ID"
+            value={loginUser.id}
+            onChange={(e) => setLoginUser({ ...loginUser, id: e.target.value })}
+            fullWidth
+            margin="dense"
+          />
+          <TextField
+            label="Password"
+            value={loginUser.password}
+            onChange={(e) => setLoginUser({ ...loginUser, password: e.target.value })}
+            type="password"
+            fullWidth
+            margin="dense"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleLogin}>Login</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
